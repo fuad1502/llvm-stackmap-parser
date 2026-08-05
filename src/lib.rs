54b3eb9c4876ee282578fdc5092b5a13de50e579
Fuad Ismail <fuad1502@gmail.com>
2026-08-05 19:23:30 +0700

    Add read reloc names function

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,40 @@ use goblin::elf::Elf;
 
 pub mod stackmap;
 
-pub fn readelf(path: &Path) -> Vec<u8> {
+pub fn read_reloc_names(path: &Path, section_name: &str) -> Vec<String> {
+    let data = fs::read(path).unwrap();
+    let elf = Elf::parse(&data).expect("Failed to parse ELF");
+
+    let reloc_section = &elf
+        .shdr_relocs
+        .iter()
+        .find(|(idx, _)| {
+            elf.shdr_strtab
+                .get_at(elf.section_headers[*idx].sh_name)
+                .unwrap_or("")
+                == section_name
+        })
+        .unwrap()
+        .1;
+
+    let mut reloc_names = vec![];
+    for reloc in reloc_section {
+        let sym = elf.syms.get(reloc.r_sym).unwrap();
+        let name = String::from(elf.strtab.get_at(sym.st_name).unwrap());
+        reloc_names.push(name);
+    }
+
+    reloc_names
+}
+
+pub fn read_section_bytes(path: &Path, section_name: &str) -> Vec<u8> {
     let data = fs::read(path).unwrap();
     let elf = Elf::parse(&data).expect("Failed to parse ELF");
 
     let section = elf
         .section_headers
         .iter()
-        .find(|section| elf.shdr_strtab.get_at(section.sh_name).unwrap_or("") == ".llvm_stackmaps")
+        .find(|section| elf.shdr_strtab.get_at(section.sh_name).unwrap_or("") == section_name)
         .unwrap();
 
     Vec::from(&data[section.sh_offset as usize..(section.sh_offset + section.sh_size) as usize])
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::path::PathBuf;
 
-use llvm_stackmap_parser::{readelf, stackmap::StackMap};
+use llvm_stackmap_parser::{read_reloc_names, read_section_bytes, stackmap::StackMap};
 
 fn main() {
     let path = PathBuf::from("/home/fuad1502/code/oonta/ocaml/merge_sort.o");
-    let bytes = readelf(&path);
+    let bytes = read_section_bytes(&path, ".llvm_stackmaps");
     let stack_map = StackMap::from(&bytes[..]);
 
     println!("{:#?}", stack_map);
+
+    println!("{:#?}", read_reloc_names(&path, ".rela.llvm_stackmaps"));
 }
